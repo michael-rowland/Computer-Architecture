3a2adf94022e0a39cf7b3df2563f3092f2f5cd84
Michael Rowland <mrowland13@gmail.com>
2020-07-21 15:49:12 -0700

    monday code challenge

---
+++ code.py
@@ -0,0 +1,27 @@
+"""
+Given the following array of values, print out all the elements in reverse order, with each element on a new line.
+For example, given the list
+[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+Your output should be
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+You may use whatever programming language you'd like.
+Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+"""
+# want to iterate over this array in reverse order
+# plan: use range() backwards
+
+array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+for i in range(10, -1, -1):
+    print(array[i])
+for i in enumerate(array, -1):
+    print(i)
